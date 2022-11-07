b5d3ac0248702ce2c1f63a51e01f8454ae2625ec
fionabaker <fionabaker@localhost>
2022-11-07 10:36:24 -0400

    Created decay plots for Co-60, Co-58 and Fe-59

--- UNB/Cobalt60.py
+++ UNB/Cobalt60.py
@@ -1,16 +1,8 @@
 '''
-Created on Nov. 4, 2022
+Created on Nov. 6, 2022
 
-@author: Fiona
-
+@author: fionabaker
 '''
-
-'''
-Created on Nov. 3, 2021
-
-@author: olgap
-'''
-
 from scipy.integrate import solve_ivp
 import radioactivedecay as rd
 import numpy as np
@@ -20,30 +12,15 @@ import matplotlib.pyplot as plot
 
 FLUX = 2e14 * 3600  # n/cm2 h
 
-ABUNDANCEFe58 = 0.00282
-ABUNDANCEFe54 = 0.0584
-ABUNDANCENi58 = 0.681
-ABUNDANCENi62 = 0.0363
-ABUNDANCECr50 = 0.0435
 ABUNDANCECo59 = 1
 
-iso_list = ['Co-60', 'Co-58'] #'Cr-51', 'Fe-59', 'Fe-55', 'Mn-54', 'Ni-63']
-Cobalt = 'Co-59'
-Nickel = 'Ni-58'
+iso_list = ['Co-60', 'Co-60']
 
+Cobalt = 'Co-59'
 
 CROSS_SECTIONCo59 = 3.7E-23 # cm^2
-CROSS_SECTIONNi58 = 1.46E-25 # cm^2
-
-CROSS_SECTIONFe58 = 1.14E-24 # cm^2
-CROSS_SECTIONFe54 = 2.5E-24 # cm^2
-CROSS_SECTIONNi62 = 1.46E-23 # cm^2
-CROSS_SECTIONCr50 = 1.59E-23 # cm^2
 
 CoDensity = 8.9 #g/cm^3
-NiDensity = 8.902 #g/cm^3
-
-
 
 def CobaltProduction(Flux_CO):
     nuc = rd.Nuclide(Cobalt)
@@ -53,16 +30,7 @@ def CobaltProduction(Flux_CO):
     
     return production_term
 
-#def NickelProduction(Flux_NI):
-  #  nuc = rd.Nuclide(Nickel)
-  #  N_atomic = NiDensity * NA * ABUNDANCENi58 / nuc.atomic_mass
-  #  macro_cross_Ni = CROSS_SECTIONNi58 * N_atomic
-  #  production_term = Flux_NI * macro_cross_Ni * FLUX
-    
-  #  return production_term
-
-
-def initial_val_decay_const(isotope):
+def initial_decay_constant(isotope):
     # based on input isotope (iso), calcs decay constant 
     nuc = rd.Nuclide(isotope)
     decay_const = np.log(2) / nuc.half_life('y') 
@@ -71,10 +39,9 @@ def initial_val_decay_const(isotope):
         input_atoms = 0
     else:
         input_atoms = 0     
-    N_0_atoms = input_atoms
+    N0_atoms = input_atoms
     
-    return N_0_atoms, decay_const
-
+    return N0_atoms, decay_const
 
 def decay_function(t, y, k1, Flux_CO):
    
@@ -83,13 +50,12 @@ def decay_function(t, y, k1, Flux_CO):
     dCo60 = P_Co59 - k1 * y[0]
     return dCo60
 
-
 #defines a function that returns the inital the number of atoms and the decay constant for each isotope
-
 #uses a for loop to obtain the data at every year for 10 years
-time = [0, 10]
+
+time = [0, 40]
 time_solutions = []
-for i in range(0, 11, 1):
+for i in range(0, 41, 1):
     time_solutions.append(i)
     
 #the for loop is used to input the initial decay of each isotope into the empty arrays
@@ -98,14 +64,14 @@ k = []
 
 # For loop used to solve ODEs
 for isotope in iso_list:
-    k.append(initial_val_decay_const(isotope)[1])
-    IV_y.append(initial_val_decay_const(isotope)[0])
+    k.append(initial_decay_constant(isotope)[1])
+    IV_y.append(initial_decay_constant(isotope)[0])
 solution = solve_ivp(decay_function, time, IV_y, t_eval=time_solutions,args=(k), dense_output = True)
 #uses the scipy.integrate function to calcule the ODE for each isotope
 
 
 #array of colors to correspond with each isotope in the isotopes list
-colours = ['mediumslateblue', 'mediumturquoise']
+colours = ['mediumslateblue', 'mediumslateblue']
 
 #for loop to plot the ODE for each isotope for the given time interval
 for answer, color in zip(solution.y, colours):
@@ -118,7 +84,7 @@ plot.ylabel('Number of Atoms/cm^3', fontdict=font, labelpad=8)
 
 #specifies the information for the legend (corresponding isotope)
 labels = ['Co-60']
-plot.legend(labels, ncol=1, edgecolor='black', loc='upper right')
+plot.legend(labels, ncol=1, edgecolor='black', loc='best')
 plot.tick_params(axis="both",direction="in")
 
 #displays the plot and saves it as a .png file
--- UNB/Iron59.py
+++ UNB/Iron59.py
@@ -1,14 +1,7 @@
 '''
-Created on Nov. 4, 2022
+Created on Nov. 7, 2022
 
-@author: Fiona
-
-'''
-
-'''
-Created on Nov. 3, 2021
-
-@author: olgap
+@author: fionabaker
 '''
 
 from scipy.integrate import solve_ivp
@@ -21,75 +14,49 @@ import matplotlib.pyplot as plot
 FLUX = 2e14 * 3600  # n/cm2 h
 
 ABUNDANCEFe58 = 0.00282
-ABUNDANCEFe54 = 0.0584
-ABUNDANCENi58 = 0.681
-ABUNDANCENi62 = 0.0363
-ABUNDANCECr50 = 0.0435
-ABUNDANCECo59 = 1
 
-iso_list = ['Co-60', 'Co-58'] #'Cr-51', 'Fe-59', 'Fe-55', 'Mn-54', 'Ni-63']
-Cobalt = 'Co-59'
-Nickel = 'Ni-58'
+iso_list = ['Fe-59', 'Fe-59']
 
-
-CROSS_SECTIONCo59 = 3.7E-23 # cm^2
-CROSS_SECTIONNi58 = 1.46E-25 # cm^2
+Iron = 'Fe-58'
 
 CROSS_SECTIONFe58 = 1.14E-24 # cm^2
-CROSS_SECTIONFe54 = 2.5E-24 # cm^2
-CROSS_SECTIONNi62 = 1.46E-23 # cm^2
-CROSS_SECTIONCr50 = 1.59E-23 # cm^2
 
-CoDensity = 8.9 #g/cm^3
-NiDensity = 8.902 #g/cm^3
+FeDensity = 7.874 #g/cm^3
 
-
-
-def CobaltProduction(Flux_CO):
-    nuc = rd.Nuclide(Cobalt)
-    N_atomic = CoDensity * NA * ABUNDANCECo59 / nuc.atomic_mass
-    macro_cross_Co = CROSS_SECTIONCo59 * N_atomic
-    production_term = Flux_CO * macro_cross_Co * FLUX
+def IronProduction(Flux_FE):
+    nuc = rd.Nuclide(Iron)
+    N_atomic = FeDensity * NA * ABUNDANCEFe58 / nuc.atomic_mass
+    macro_cross_Co = CROSS_SECTIONFe58 * N_atomic
+    production_term = Flux_FE * macro_cross_Co * FLUX
     
     return production_term
 
-#def NickelProduction(Flux_NI):
-  #  nuc = rd.Nuclide(Nickel)
-  #  N_atomic = NiDensity * NA * ABUNDANCENi58 / nuc.atomic_mass
-  #  macro_cross_Ni = CROSS_SECTIONNi58 * N_atomic
-  #  production_term = Flux_NI * macro_cross_Ni * FLUX
-    
-  #  return production_term
-
-
-def initial_val_decay_const(isotope):
+def initial_decay_constant(isotope):
     # based on input isotope (iso), calcs decay constant 
     nuc = rd.Nuclide(isotope)
-    decay_const = np.log(2) / nuc.half_life('y') 
+    decay_const = np.log(2) / nuc.half_life('d') 
   
     if isotope == iso_list[0]:
         input_atoms = 0
     else:
         input_atoms = 0     
-    N_0_atoms = input_atoms
+    N0_atoms = input_atoms
     
-    return N_0_atoms, decay_const
+    return N0_atoms, decay_const
 
-
-def decay_function(t, y, k1, Flux_CO):
+def decay_function(t, y, k1, Flux_FE):
    
-    P_Co59 = CobaltProduction(Flux_CO)
+    P_Fe58 = IronProduction(Flux_FE)
 
-    dCo60 = P_Co59 - k1 * y[0]
-    return dCo60
-
+    dFe59 = P_Fe58 - k1 * y[0]
+    return dFe59
 
 #defines a function that returns the inital the number of atoms and the decay constant for each isotope
-
 #uses a for loop to obtain the data at every year for 10 years
-time = [0, 10]
+
+time = [0, 600]
 time_solutions = []
-for i in range(0, 11, 1):
+for i in range(0, 601, 1):
     time_solutions.append(i)
     
 #the for loop is used to input the initial decay of each isotope into the empty arrays
@@ -98,14 +65,14 @@ k = []
 
 # For loop used to solve ODEs
 for isotope in iso_list:
-    k.append(initial_val_decay_const(isotope)[1])
-    IV_y.append(initial_val_decay_const(isotope)[0])
+    k.append(initial_decay_constant(isotope)[1])
+    IV_y.append(initial_decay_constant(isotope)[0])
 solution = solve_ivp(decay_function, time, IV_y, t_eval=time_solutions,args=(k), dense_output = True)
 #uses the scipy.integrate function to calcule the ODE for each isotope
 
 
 #array of colors to correspond with each isotope in the isotopes list
-colours = ['mediumslateblue', 'mediumturquoise']
+colours = ['mediumblue', 'mediumblue']
 
 #for loop to plot the ODE for each isotope for the given time interval
 for answer, color in zip(solution.y, colours):
@@ -113,12 +80,12 @@ for answer, color in zip(solution.y, colours):
 
 #specifies the font and titles of the x and y labels
 font = {'family': 'arial', 'color': 'black', 'weight': 'normal', 'size': 12,}
-plot.xlabel('Time (years)', fontdict = font, labelpad=8)
+plot.xlabel('Time (days)', fontdict = font, labelpad=8)
 plot.ylabel('Number of Atoms/cm^3', fontdict=font, labelpad=8)
 
 #specifies the information for the legend (corresponding isotope)
-labels = ['Co-60']
-plot.legend(labels, ncol=1, edgecolor='black', loc='upper right')
+labels = ['Fe-59']
+plot.legend(labels, ncol=1, edgecolor='black', loc='best')
 plot.tick_params(axis="both",direction="in")
 
 #displays the plot and saves it as a .png file
